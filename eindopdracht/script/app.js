a9f92f77119a15bbd35135caacfb271e099c7f61
JonasVS <jonas.van.slambrouck@student.howest.be>
2021-12-08 18:00:10 +0100

    fix: no duplicates

--- eindopdracht/script/app.js
+++ eindopdracht/script/app.js
@@ -2,10 +2,12 @@
 
 let htmlada, htmlbtc, htmlxrp, htmlsol, htmleth;
 let htmlchart_ada, html_grid;
-let html_addbutton;
-var price, lastprice1 = 0, lastprice2 = 0, lastprice3 = 0, lastprice4 = 0, lastprice5 = 0;
+let html_addbutton, colloring;
+var price, lastprice = 0;
 var cryptolist = ["ADAUSD", "XXBTZUSD", "SOLUSD", "XETHZUSD", "XXRPZUSD", "USDTZUSD", "DOTUSD", "XDGUSD", "UNIUSD", "LINKUSD"];
-let stat = "";
+var amount = 0;
+var amount_assets = 0;
+var asset_new = "";
 
 function drawChart(labels, data, classchart) {
     var options = {
@@ -24,14 +26,18 @@ function drawChart(labels, data, classchart) {
             }
         },
         tooltip: {
+            enabled: true,
             theme: "dark",
             colors: "",
             style: {
                 fontSize: '14px',
-              },
+            },
             x:{
                 show: false,
             },
+            y:{
+                show: false,
+            }
         },
         colors: ["#BA42F1"],
         fill: {
@@ -83,12 +89,16 @@ function drawChart(labels, data, classchart) {
 
     var chart = new ApexCharts(document.querySelector(classchart), options);
     chart.render();
+    amount += 1;
+    if(amount == amount_assets){
+        //console.log("yes???")
+        button_listener();
+    }
 }
-
+    
 const showData = function(data, coin){
     let timestamp = [];
     let historicalData = [];
-    console.log("coin: " + coin);
     for (const prices of data.result[coin]){
         timestamp.push(prices[0]);
         if(prices[5] != 0){
@@ -98,17 +108,24 @@ const showData = function(data, coin){
             historicalData.push(prices[4]);
         }
     }
-    drawChart(timestamp, historicalData, ".js-chart-" + coin);
+    if (document.querySelector(".js-chart-" + coin)){
+        drawChart(timestamp, historicalData, ".js-chart-" + coin);
+    }
+    else{
+        setTimeout(() => {  showData(data, coin); }, 1000);
+    }
 }
 
-function getHistoricalData(coinlist){
-    coinlist.forEach(element => {
-        fetch('https://api.kraken.com/0/public/OHLC?pair=' + element + '&interval=60')
-        .then(response => response.json())
-        .then(data => {
-            showData(data, element);
+function getHistoricalData(coinlist, state){
+    setTimeout(function() {
+        coinlist.forEach(element => {
+            fetch('https://jonasvscryptoapi.azurewebsites.net/api/OHLC/' + element)
+            .then(response => response.json())
+            .then(data => {
+                showData(data, element);
+            });
         });
-    });
+      }, 1000);
 }
 
 function convertToInternationalCurrencySystem (labelValue) {
@@ -144,9 +161,9 @@ function percIncrease(a, b) {
     return percent.toFixed(4);
 }
 
-function getCurrentData(coinlist){
-    coinlist.forEach(element => {
-        fetch('https://api.kraken.com/0/public/Ticker?pair=' + element)
+async function getCurrentData(coinlist, isnew){
+    await coinlist.forEach(element => {
+        fetch('https://jonasvscryptoapi.azurewebsites.net/api/ticker/' + element)
         .then(response => response.json())
         .then(data => {
             if(Object.keys(data.result)[0] != element){
@@ -157,11 +174,14 @@ function getCurrentData(coinlist){
             let vol = data.result[element].v[1];
             let opening = data.result[element].o;
             let change = (price - opening);
+            asset_new = element;
+            if(isnew == true){
+                cryptolist.push(asset_new);
+            }
             if(change > 0){
                 change == "+" + change
             }
 
-            let colloring
             if(change >= 0){
                 colloring = "#70E000";
             }
@@ -170,33 +190,32 @@ function getCurrentData(coinlist){
             }
 
             let changeperc = percIncrease(opening, price);
-            console.log("price " + element + ": " + price);
+            console.log("price " + element + ": $" + price);
 
-            if(lastprice1 != 0){
+            if(lastprice != 0){
 
-                if(price < lastprice1){
+                if(price < lastprice){
 
                 }
-                else if(price > lastprice1){
+                else if(price > lastprice){
 
                 }
             }
-            lastprice1 = price;
+            lastprice = price;
             
             try {
-                console.log("element: " + element);
                 html_grid.innerHTML += `
                 <div class="o-grid-item">
-                    <img class="c-logo" src='svg/${element}.svg' alt="some file"  height='100'width='100' style="color:green;"/>
-                    <div style="text-align: right; float: right; margin: 0 16px 0 0;">
-                        <h1 class="c-card__title">
+                    <img class="c-logo" src='svg/${element}.svg' alt="some file"  height='100'width='100' style="color:green;" onerror="this.onerror=null; this.src='svg/safari-pinned-tab.svg'"/>
+                    <div class="c-assetNamePrice">
+                        <h1 class="c-card__title js-assetname">
                             ${element}
                         </h1>
                         <h3 class="c-card__price" style="color:${colloring}">
                             ${price}
                         </h3>
                     </div>
-                    <div style="text-align: right; float: left; margin: 0 0 0 0;">
+                    <div class="c-assetinfo">
                         <h1 class="c-card__fullname">
                             ${element.slice(0, element.length-3)}
                         </h1>
@@ -204,17 +223,16 @@ function getCurrentData(coinlist){
                             vol: ${convertToInternationalCurrencySystem (vol)}
                         </h3>
                         <h3 class="c-card__change" style="color:${colloring}">
-                            ${change.toFixed(6)}
+                            chg: ${change.toFixed(6)}
                         </h3>
                         <h3 class="c-card__change" style="color:${colloring}">
-                            ${changeperc}%
+                            chg%: ${changeperc}%
                         </h3>
                     </div>
-                    <div style="position:absolute; left: -10px; bottom: -15px; width: 104%;" id="chart" class="js-chart-${element}"></div>
+                    <div style="position:relative; left: -32px; " id="chart" class="js-chart-${element}" c-chart></div>
                 </div>`
             }
             catch(err) {
-                console.log("no svg found");
                 html_grid.innerHTML += `
                 <div class="o-grid-item">
                     <img class="c-logo" src='svg/ADAUSD.svg' alt="some file"  height='100'width='100' style="color:green;"/>
@@ -226,7 +244,7 @@ function getCurrentData(coinlist){
                             ${price}
                         </h3>
                     </div>
-                    <div style="text-align: right; float: left; margin: 0 0 0 0;">
+                    <div class="c-assetinfo">
                         <h1 class="c-card__fullname">
                             Cardano
                         </h1>
@@ -245,76 +263,67 @@ function getCurrentData(coinlist){
             }
         });
     });
-    
-
-    console.log("done loading data, now waiting 5 sec for reload!")
-    stat = "done";
+    if(isnew == true){
+        cryptolist.push(asset_new);
+    }
 }
 
 function button_listener(){
     html_addbutton = document.querySelector('.js-add-asset');
     let asset;
     html_addbutton.addEventListener("click", function(){
-        asset = prompt("Please enter the ticker symbol of your choice:", "LTC");
+        asset = prompt("Please enter the Kraken ticker symbol of your choice:", "XLTCZ");
+        let assetname = document.querySelectorAll('.js-assetname');
+        let isonpage = false;
+        for(const name of assetname){
+            if(name.innerHTML.includes(asset)){
+                isonpage = true;
+            }
+        }
+        
         if(asset == null){
             console.log("retuned null");
-            window.alert("sometext");
+            window.alert("Canceled");
         }
-        else if(cryptolist.indexOf(asset + "USD") !== -1){
+        else if(isonpage == true){
             window.alert("This ticker symbol already is on this page!");
         }
         else{
-            console.log("asset: " + asset);
-            cryptolist.push(asset + "USD");
+            amount_assets += 1;
+            console.log("adding asset: " + asset);
             var new_asset = [asset + "USD"];
             getCurrentData(new_asset);
-            var kraken_asset = [cryptolist[cryptolist.length - 1]];
-            setTimeout(() => {button_listener(); }, 500);
-            setTimeout(() => {getHistoricalData(kraken_asset); }, 1000);
+
+            setTimeout(function(){
+                //console.log("assets: " + cryptolist)
+                var kraken_asset = [asset_new];
+                window.scrollTo({ top: document.body.scrollHeight, behavior: 'smooth' })
+                getHistoricalData(kraken_asset, true);
+            }, 1000);
         }
     });
 } 
 
 document.addEventListener('DOMContentLoaded', function () {
     console.log('Script loaded!');
+    amount_assets = cryptolist.length;
     html_grid = document.querySelector('.js-grid');
     html_grid.innerHTML += `
-                <div class="o-grid-item js-add-asset" style="display: flex;">
+                <div class="o-grid-item js-add-asset o-grid-item__add">
                     <svg class="c-add-asset o-grow" xmlns="http://www.w3.org/2000/svg" width="152" height="152" viewBox="0 0 152 152">
                         <g id="Group_18" data-name="Group 18" transform="translate(-8 -10)">
-                        <g id="Ellipse_1" data-name="Ellipse 1" transform="translate(8 10)" fill="none" stroke="#223154" stroke-width="15">
+                        <g id="Ellipse_1" data-name="Ellipse 1" transform="translate(8 10)" fill="none" stroke="#03122c" stroke-width="15">
                             <circle cx="76" cy="76" r="76" stroke="none"/>
                             <circle cx="76" cy="76" r="68.5" fill="none"/>
                         </g>
                         <g id="Group_19" data-name="Group 19" transform="translate(76.244 76.822)">
-                            <line id="Line_7" data-name="Line 7" y2="91" transform="translate(7.756 -35.822)" fill="none" stroke="#223154" stroke-linecap="round" stroke-width="15"/>
-                            <line id="Line_8" data-name="Line 8" x2="92" transform="translate(-38.244 9.178)" fill="none" stroke="#223154" stroke-linecap="round" stroke-width="15"/>
+                            <line id="Line_7" data-name="Line 7" y2="91" transform="translate(7.756 -35.822)" fill="none" stroke="#03122c" stroke-linecap="round" stroke-width="15"/>
+                            <line id="Line_8" data-name="Line 8" x2="92" transform="translate(-38.244 9.178)" fill="none" stroke="#03122c" stroke-linecap="round" stroke-width="15"/>
                         </g>
                         </g>
-                    </svg>                  
+                    </svg>  
                 </div>`;
-    setTimeout(() => {button_listener(); }, 500);
+    button_listener();
     getCurrentData(cryptolist);
-    setTimeout(() => {getHistoricalData(cryptolist); }, 1000);
-    setInterval(function(){ 
-        html_grid.innerHTML = ``
-        html_grid.innerHTML += `
-                <div class="o-grid-item js-add-asset" style="display: flex;">
-                    <svg class="c-add-asset o-grow" xmlns="http://www.w3.org/2000/svg" width="152" height="152" viewBox="0 0 152 152">
-                        <g id="Group_18" data-name="Group 18" transform="translate(-8 -10)">
-                        <g id="Ellipse_1" data-name="Ellipse 1" transform="translate(8 10)" fill="none" stroke="#223154" stroke-width="15">
-                            <circle cx="76" cy="76" r="76" stroke="none"/>
-                            <circle cx="76" cy="76" r="68.5" fill="none"/>
-                        </g>
-                        <g id="Group_19" data-name="Group 19" transform="translate(76.244 76.822)">
-                            <line id="Line_7" data-name="Line 7" y2="91" transform="translate(7.756 -35.822)" fill="none" stroke="#223154" stroke-linecap="round" stroke-width="15"/>
-                            <line id="Line_8" data-name="Line 8" x2="92" transform="translate(-38.244 9.178)" fill="none" stroke="#223154" stroke-linecap="round" stroke-width="15"/>
-                        </g>
-                        </g>
-                    </svg>                  
-                </div>`;
-    setTimeout(() => {button_listener(); }, 500);
-    getCurrentData(cryptolist);
-    setTimeout(() => {getHistoricalData(cryptolist); }, 1000);
-    }, 30000);
+    getHistoricalData(cryptolist);
 });
